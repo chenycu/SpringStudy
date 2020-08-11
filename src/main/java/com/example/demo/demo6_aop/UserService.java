5463a4ae3467293b83beeb0cc846f8ae925b7d84
cheny <you92@vip.qq.com>
2020-08-12 00:17:15 +0800

    小修改

---
+++ src/main/java/com/example/demo/demo6_aop/UserService.java
@@ -0,0 +1,8 @@
+package com.example.demo.demo6_aop;
+
+public class UserService {
+
+    public void work(String username) {
+        System.out.println(username + "正在学习中。。。");
+    }
+}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,11 +4,20 @@ import com.example.demo.demo3_jdbctemplate.StudentService;
 import com.example.demo.demo5_circle_dependes.ScanConfig;
 import com.example.demo.demo5_circle_dependes.ServiceA;
 import com.example.demo.demo5_circle_dependes.ServiceB;
+import com.example.demo.demo6_aop.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodBeforeAdvice;
+import org.springframework.aop.MethodMatcher;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.lang.reflect.Method;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -41,7 +50,47 @@ class DemoApplicationTests {
 
     @Test
     public void test3() {
-        String a = "bbg";
-        System.out.println(a instanceof Object);
+        UserService target = new UserService();
+        Pointcut pointcut = new Pointcut() {
+            @Override
+            public ClassFilter getClassFilter() {
+                return clazz -> UserService.class.isAssignableFrom(clazz);
+            }
+
+            @Override
+            public MethodMatcher getMethodMatcher() {
+                return new MethodMatcher() {
+                    @Override
+                    public boolean matches(Method method, Class<?> targetClass) {
+                        return "work".equals(method.getName());
+                    }
+
+                    @Override
+                    public boolean isRuntime() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean matches(Method method, Class<?> targetClass, Object... args) {
+                        return false;
+                    }
+                };
+            }
+        };
+
+        MethodBeforeAdvice advice = new MethodBeforeAdvice() {
+            @Override
+            public void before(Method method, Object[] args, Object target) throws Throwable {
+                System.out.println("before 。。。arg[0]=" + args[0]);
+            }
+        };
+
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(pointcut, advice);
+
+        ProxyFactory proxyFactory = new ProxyFactory();
+        proxyFactory.setTarget(target);
+        proxyFactory.addAdvisor(advisor);
+        UserService userServiceProxy = (UserService) proxyFactory.getProxy();
+        userServiceProxy.work("cheny");
     }
 }
